2f7abc1a4ca5cb26dd0df06dbaac100042971eea
Nick Bratt <nrbratt@neiu.edu>
2021-02-17 17:42:56 -0600

    Initial commit

---
+++ src/main/java/com/tpsc/thepeoplesscorecard/ThePeoplesScoreCardApplication.java
@@ -0,0 +1,14 @@
+package com.tpsc.thepeoplesscorecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePeoplesScoreCardApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ThePeoplesScoreCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tpsc/thepeoplesscorecard/ThePeoplesScoreCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpsc.thepeoplesscorecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePeoplesScoreCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
